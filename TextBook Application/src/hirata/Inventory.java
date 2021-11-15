e7c6d9e3bf489fa0f40f5018e15405dcb4a8bba4
yurikahirata <yurikahirata0130@gmail.com>
2021-11-14 20:46:04 -0800

    Removed unnecessary imports

--- TextBook Application/src/hirata/Inventory.java
+++ TextBook Application/src/hirata/Inventory.java
@@ -1,9 +1,7 @@
 package hirata;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
